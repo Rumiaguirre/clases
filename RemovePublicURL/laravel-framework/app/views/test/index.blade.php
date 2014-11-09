59e76a4a2420f0907ed7ee7505be5d616d4cbde1
Rumi Aguirre <rumi_1994@hotmail.com>
2014-11-09 13:42:25 +0100

    moved framework to own folder

---
+++ RemovePublicURL/laravel-framework/app/views/test/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Clases particulares</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h1>Test view</h1>
+		<h2>{{ $var1 }}</h2>
+		<img src="{{ URL::asset('assets/img/img.png') }}" alt="" class="">
+	</div>
+</body>
+</html>
